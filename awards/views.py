1d058651e4924df0fe0b1e60dd7e44733d57aa62
DjCooGie <evansonchagwa01@gmail.com>
2020-02-16 22:14:44 +0300

    create register view function responsible for displaying the registration/login pages

--- awards/views.py
+++ awards/views.py
@@ -13,3 +13,15 @@ def home(request):
     return render(request, 'index.html')
 
 
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account for {username} has been created successfully')
+            return redirect('/')
+        
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/registration_form.html', {'form':form})
