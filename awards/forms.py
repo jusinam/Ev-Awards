1bb2ccad8fba0cf99eec6419ae443d2f181727c9
DjCooGie <evansonchagwa01@gmail.com>
2020-02-16 15:03:29 +0300

    create forms file with imports responsible for creating form functions

---
+++ awards/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from crispy_forms.helper import FormHelper
+from registration.forms import RegistrationForm
