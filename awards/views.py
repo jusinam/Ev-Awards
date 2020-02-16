aea61331fdd8c1bb1cb5058a1ee01e390305bf74
DjCooGie <evansonchagwa01@gmail.com>
2020-02-16 22:12:50 +0300

    create homepage view function

--- awards/views.py
+++ awards/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render, redirect, render_to_response, HttpResponseR
 from django.conf import settings
 from django.templatetags.static import static
 from django.http import HttpResponse, Http404
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 from .models import *
 from .forms import *
 
 # Create your views here.
+def home(request): 
+
+    return render(request, 'index.html')
+
 
