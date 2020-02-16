72d6415e8925c873e2ca6874fbcf55d6e7b9d4ce
DjCooGie <evansonchagwa01@gmail.com>
2020-02-16 15:39:12 +0300

    add base imports

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, render_to_response, HttpResponseRedirect
+from django.conf import settings
+from django.templatetags.static import static
+from django.http import HttpResponse, Http404
+from .models import *
+from .forms import *
 
 # Create your views here.
+
