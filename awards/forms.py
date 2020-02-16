9a417382a7aea91813158b18ef7ef0def6079a95
DjCooGie <evansonchagwa01@gmail.com>
2020-02-16 22:10:53 +0300

    create registeForm function by passing in an object of the django inbuilt RegistrationForm function

--- awards/forms.py
+++ awards/forms.py
@@ -3,3 +3,16 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from crispy_forms.helper import FormHelper
 from registration.forms import RegistrationForm
+
+class RegisterForm(RegistrationForm):
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2',)
+        
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+        self.helper.form_show_labels = True 
