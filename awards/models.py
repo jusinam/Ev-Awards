92ca21e1238832f1cf68566a45f559b47149bffa
DjCooGie <evansonchagwa01@gmail.com>
2020-02-17 03:32:13 +0300

    create Project model class and add four method function that work hand in hand in displaying projects by search

--- awards/models.py
+++ awards/models.py
@@ -2,6 +2,8 @@ from django.db import models
 import datetime as dt
 from django.contrib.auth.models import User
 from PIL import Image
+from django_countries.fields import CountryField
+from star_ratings.models import Rating
 
 # Create your models here.
 
@@ -29,3 +31,55 @@ class Profile(models.Model):
     class Meta:
         verbose_name = 'Profile'
         verbose_name_plural = 'Profiles'
+
+class Project(models.Model):
+    project_title = models.CharField(max_length=255)
+    project_image = models.ImageField(upload_to = 'images/', default='images/default.jpg')
+    project_description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    Author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    author_profile = models.ForeignKey(Profile,on_delete=models.CASCADE, blank=True, default='1')
+    link = models.URLField()
+    country = CountryField(blank_label='(select country)', default='KE')
+ 
+        
+    def save_project(self):
+        self.save()
+    
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def get_projects(cls):
+        projects = cls.objects.all()
+        return projects
+    
+    @classmethod
+    def search_projects(cls, search_term):
+        projects = cls.objects.filter(p_title__icontains=search_term)
+        return projects
+    
+    
+    @classmethod
+    def get_by_author(cls, Author):
+        projects = cls.objects.filter(Author=Author)
+        return projects
+    
+    
+    @classmethod
+    def get_project(request, id):
+        try:
+            project = Project.objects.get(pk = id)
+            
+        except ObjectDoesNotExist:
+            raise Http404()
+        
+        return project
+    
+    def __str__(self):
+        return self.project_title
+    
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
